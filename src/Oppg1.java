36e388b5916021b996faa4135276b42ad9d46fee
sigrun <slindaas@gmail.com>
2014-10-03 11:51:32 +0200

    Oppdatert første opp

--- src/Oppg1.java
+++ src/Oppg1.java
@@ -1,8 +1,17 @@
+import javax.swing.JOptionPane;
+
 
 public class Oppg1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String utskrift = "";//tom utskrift for å?
+		
+		for ( int i = 1; i <100; i++ )
+		    
+			utskrift += i + "\n";
+		   
+		
+		JOptionPane.showMessageDialog(null, utskrift);
 
 	}
 
